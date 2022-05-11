6bfdc9219f216447934e562ffd93f8d476bb4238
nahid-35 <nahid35@diit.info>
2022-05-11 21:59:26 +0600

    Modification added

--- min_max.cpp
+++ min_max.cpp
@@ -1,4 +1,3 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -9,52 +8,48 @@ int main() {
     // Write C++ code here
     
     vector<int> arr;
-    arr.push_back(140638725);
-    arr.push_back(436257910);
-    arr.push_back(953274816);
-    arr.push_back(734065819);
-    arr.push_back(362748590);
+    arr.push_back(5);
+    arr.push_back(5);
+    arr.push_back(5);
+    arr.push_back(5);
+    arr.push_back(5);
     
-    //140638725 436257910 953274816 734065819 362748590
-    
+       
     unsigned long int max=0;
     unsigned long int min=0;
-    vector<int> nums;
-    unsigned long int prev_min=0;
+    vector< unsigned long int> nums;
     for(int i=0;i<5;i++)
     {
         if(arr[i]>=1){
             unsigned long int sum=0;
-            unsigned long int curr_num=arr[i];
+            unsigned long int curr_num=i;
             
             for(int j=0;j<5;j++)
             {
-                if(curr_num!=arr[j])
-                { 
-                    sum+=arr[j];
-                    //cout << arr[j] << endl;
-                }
-            }
-            //cout << " " << endl;
-            
-            if(sum>max)
-            {
-                max=sum;
-            }
-            
-            if(sum<max)
-            {
-                min=sum;
-                prev_min=min;
-                if(sum<prev_min)
+                if(curr_num!=j)
                 {
-                    min=prev_min;
+                    sum+=arr[j];
                 }
-                cout << min << endl;
             }
+           nums.push_back(sum);
+
         }
     }
-    //cout << min << " " << max << endl;
+    
+    min=nums[0];
+    max=nums[0];
+    for(int j=0;j<nums.size();j++)
+    {
+        if(nums[j]<min)
+        {
+            min=nums[j];
+        }
+        if(nums[j]>max)
+        {
+            max=nums[j];
+        }
+    }
+    cout << min << " " << max << endl;
     
     return 0;
 }
